1ac85207452e96f68f30d37325a834ef6824d879
piotrneumann <asdf1@op.pl>
2020-03-25 12:14:54 +0100

    next results

--- webTest.js
+++ webTest.js
@@ -1,41 +1,35 @@
 const WebPageTest = require("webpagetest");
 const wpt = new WebPageTest("www.webpagetest.org");
+const urlPageUnderTest = "https://przyprawytobago.pl";
 
-let options = {
-    key: "A.8accedfc59e91daf4c17874479bd5c52",
-    firstViewOnly: true,
-    runs: 1,
-    pollResults: 5,
-};
 
-let id;
 
-// wpt.runTest("https://przyprawytobago.pl", options, (err, result) => {
-//
-//     // The test ID is returned in the response here:
-//     this.id = result.data.testId;
-//     console.log(err || result);
-//     console.log(this.id)
-//
-//     wpt.getTestResults(this.id, (err, res) => {
-//         console.log(err || res);
-//     });
-// });
+function runWebPageTest(page, location) {
 
-wpt.runTest('https://przyprawytobago.pl', options, function processTestResult(err, result) {
-    // First view — use `repeatView` for repeat view
-    console.log('Load time:', result.data.average.firstView.loadTime)
-    console.log('First byte:', result.data.average.firstView.TTFB)
-    console.log('Start render:', result.data.average.firstView.render)
-    console.log('Speed Index:', result.data.average.firstView.SpeedIndex)
-    console.log('DOM elements:', result.data.average.firstView.domElements)
+    let options = {
+        key: "A.8accedfc59e91daf4c17874479bd5c52",
+        firstViewOnly: true,
+        runs: 1,
+        pollResults: 5,
+        location: location,
+    };
+    wpt.runTest(urlPageUnderTest, options, function processTestResult(err, result) {
+        // First view — use `repeatView` for repeat view
+        console.log('Load time:', result.data.average.firstView.loadTime)
+        console.log('First byte:', result.data.average.firstView.TTFB)
+        console.log('Start render:', result.data.average.firstView.render)
+        console.log('Speed Index:', result.data.average.firstView.SpeedIndex)
+        console.log('DOM elements:', result.data.average.firstView.domElements)
 
-    console.log('(Doc complete) Requests:', result.data.average.firstView.requestsDoc)
-    console.log('(Doc complete) Bytes in:', result.data.average.firstView.bytesInDoc)
+        console.log('(Doc complete) Requests:', result.data.average.firstView.requestsDoc)
+        console.log('(Doc complete) Bytes in:', result.data.average.firstView.bytesInDoc)
 
-    console.log('(Fully loaded) Time:', result.data.average.firstView.fullyLoaded)
-    console.log('(Fully loaded) Requests:', result.data.average.firstView.requestsFull)
-    console.log('(Fully loaded) Bytes in:', result.data.average.firstView.bytesIn)
+        console.log('(Fully loaded) Time:', result.data.average.firstView.fullyLoaded)
+        console.log('(Fully loaded) Requests:', result.data.average.firstView.requestsFull)
+        console.log('(Fully loaded) Bytes in:', result.data.average.firstView.bytesIn)
 
-    console.log('Waterfall view:', result.data.runs[1].firstView.images.waterfall)
-})
+        console.log('Waterfall view:', result.data.runs[1].firstView.images.waterfall)
+    })
+}
+
+runWebPageTest(urlPageUnderTest, locationParam);
